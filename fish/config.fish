dddeaf9bdf34f08ec0041f3e8c43c298737ef153
sweetssuffer <vozmiisdayege@gmail.com>
2026-01-25 17:41:23 +0500

    Deleted needless functional

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,3 @@ alias e="exit"
 alias c="clear"
 alias conf="nvim ~/.config"
 alias hi="sudo env WAYLAND_DISPLAY=$WAYLAND_DISPLAY XDG_SESSION_TYPE=$XDG_SESSION_TYPE XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR hiddify"
-
-# быстрые команды для управления zapret
-alias zapret-config='$HOME/zapret-configs/install.sh'
-alias zapret-utils='$HOME/zapret-configs/utils-zapret.sh'
